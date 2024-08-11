5838a01233686a71f179ca31eac30c49a3100f4d
Gayeon07 <lgy3131@naver.com>
2024-08-11 15:06:10 +0900

    page

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,20 +1,69 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Location from './pages/Location';
+import './App.css';
 
 function App() {
-  const [message, setMessage]=useState([]);
-  useEffect(()=>{
+  const [message, setMessage] = useState([]);
+  const [activeLink, setActiveLink] = useState("/");
+
+  useEffect(() => {
     fetch("/api/demo-web")
-        .then((response)=>{
-          return response.json();
-        })
-        .then((data)=>{
-            setMessage(data);
-        });
-  },[]);
+      .then((response) => response.json())
+      .then((data) => setMessage(data));
+  }, []);
+
   return (
-    <div>
-        {message}
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link
+            to="/"
+            className={activeLink === "/" ? "active" : ""}
+            onClick={() => setActiveLink("/")}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/1"
+            className={activeLink === "/1" ? "active" : ""}
+            onClick={() => setActiveLink("/1")}
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/2"
+            className={activeLink === "/2" ? "active" : ""}
+            onClick={() => setActiveLink("/2")}
+          >
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/3"
+            className={activeLink === "/3" ? "active" : ""}
+            onClick={() => setActiveLink("/3")}
+          >
+            Location
+          </Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/1" element={<About />} />
+        <Route path="/2" element={<Contact />} />
+        <Route path="/3" element={<Location />} />
+      </Routes>
+    </Router>
   );
 }
 
