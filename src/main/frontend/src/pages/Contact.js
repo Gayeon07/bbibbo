1fbb659d62de78cd3cc6aab18696aae81fccafb4
Gayeon07 <lgy3131@naver.com>
2024-08-11 15:07:52 +0900

    page

---
+++ src/main/frontend/src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>This is contact page.</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/main/frontend/src/pages/Location.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Location() {
+  return (
+    <div>
+      <h1>Location Page</h1>
+      <p>This is location page.</p>
+    </div>
+  );
+}
+
+export default Location;
